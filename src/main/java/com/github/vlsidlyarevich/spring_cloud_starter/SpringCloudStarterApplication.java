9858fa6bd424c2914834b70c720036e4613d0dbf
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-11-30 22:54:00 +0300

    Config server implementation

--- src/main/java/com/github/vlsidlyarevich/spring_cloud_starter/SpringCloudStarterApplication.java
+++ src/main/java/com/github/vlsidlyarevich/spring_cloud_starter/SpringCloudStarterApplication.java
@@ -2,11 +2,13 @@ package com.github.vlsidlyarevich.spring_cloud_starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringCloudStarterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudStarterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStarterApplication.class, args);
+    }
 }
