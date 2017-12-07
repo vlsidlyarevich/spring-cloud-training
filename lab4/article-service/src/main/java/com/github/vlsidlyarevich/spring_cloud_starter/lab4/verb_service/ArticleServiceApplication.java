2255b2703c69809a2c7250f559f536d807a99d38
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-12-07 19:19:28 +0300

    Lab-4 - Added empty Eureka client services

---
+++ lab4/article-service/src/main/java/com/github/vlsidlyarevich/spring_cloud_starter/lab4/verb_service/ArticleServiceApplication.java
@@ -0,0 +1,14 @@
+package com.github.vlsidlyarevich.spring_cloud_starter.lab4.verb_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ArticleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleServiceApplication.class, args);
+    }
+}
