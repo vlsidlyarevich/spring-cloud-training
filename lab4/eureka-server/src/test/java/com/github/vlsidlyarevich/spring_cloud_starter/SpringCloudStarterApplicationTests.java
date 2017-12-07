1dc37dcb5957a8b6ef13a01184746c84d79f7ca4
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-12-07 19:03:33 +0300

    Lab-4 - Added Eureka config server

---
+++ lab4/eureka-server/src/test/java/com/github/vlsidlyarevich/spring_cloud_starter/SpringCloudStarterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.vlsidlyarevich.spring_cloud_starter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudStarterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
