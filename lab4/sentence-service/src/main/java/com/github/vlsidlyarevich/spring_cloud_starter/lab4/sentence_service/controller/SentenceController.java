14fdb9aa4123660e47d496f7bfea70f9486124b6
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-12-10 17:35:03 +0300

    Lab-4 - Sentence microservice is done.

--- lab4/sentence-service/src/main/java/com/github/vlsidlyarevich/spring_cloud_starter/lab4/sentence_service/controller/SentenceController.java
+++ lab4/sentence-service/src/main/java/com/github/vlsidlyarevich/spring_cloud_starter/lab4/sentence_service/controller/SentenceController.java
@@ -1,20 +1,20 @@
 package com.github.vlsidlyarevich.spring_cloud_starter.lab4.sentence_service.controller;
 
+import com.github.vlsidlyarevich.spring_cloud_starter.lab4.sentence_service.service.SentenceService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@AllArgsConstructor(onConstructor = @Autowired)
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class SentenceController {
 
-    private final DiscoveryClient discoveryClient;
+    private final SentenceService sentenceService;
 
-    @GetMapping("/")
+    @GetMapping("/sentence")
     public ResponseEntity getSentence() {
-
+        return ResponseEntity.ok().body(sentenceService.getWord());
     }
 }
---
+++ lab4/sentence-service/src/main/java/com/github/vlsidlyarevich/spring_cloud_starter/lab4/sentence_service/service/SentenceService.java
@@ -0,0 +1,6 @@
+package com.github.vlsidlyarevich.spring_cloud_starter.lab4.sentence_service.service;
+
+public interface SentenceService {
+
+    String getWord();
+}
