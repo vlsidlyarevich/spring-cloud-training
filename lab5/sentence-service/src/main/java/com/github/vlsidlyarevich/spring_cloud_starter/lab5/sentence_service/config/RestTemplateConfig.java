c6e06f0dbaa82426c19e4fef69ac8b16e0c8b569
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-12-16 22:03:16 +0300

    Lab-5 - Added Ribbon load balancing

---
+++ lab5/sentence-service/src/main/java/com/github/vlsidlyarevich/spring_cloud_starter/lab5/sentence_service/config/RestTemplateConfig.java
@@ -0,0 +1,16 @@
+package com.github.vlsidlyarevich.spring_cloud_starter.lab5.sentence_service.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
--- lab5/sentence-service/src/main/java/com/github/vlsidlyarevich/spring_cloud_starter/lab5/sentence_service/service/HttpSentenceService.java
+++ lab5/sentence-service/src/main/java/com/github/vlsidlyarevich/spring_cloud_starter/lab5/sentence_service/service/HttpSentenceService.java
@@ -3,7 +3,6 @@ package com.github.vlsidlyarevich.spring_cloud_starter.lab5.sentence_service.ser
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,34 +14,26 @@ import java.util.Objects;
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class HttpSentenceService implements SentenceService {
 
+    private final String HTTP_PREFIX = "http://";
     private final String SUBJECT_SERVICE = "subject-service";
     private final String VERB_SERVICE = "verb-service";
     private final String ARTICLE_SERVICE = "article-service";
     private final String ADJECTIVE_SERVICE = "adjective-service";
     private final String NOUN_SERVICE = "noun-service";
 
-    private final DiscoveryClient discoveryClient;
+    private final RestTemplate restTemplate;
 
     @Override
     public String getWord() {
-        return getWord(SUBJECT_SERVICE) + " "
-                + getWord(VERB_SERVICE) + " "
-                + getWord(ARTICLE_SERVICE) + " "
-                + getWord(ADJECTIVE_SERVICE) + " "
-                + getWord(NOUN_SERVICE) + ".";
+        return String.format("%s %s %s %s %s.",
+                getWord(SUBJECT_SERVICE),
+                getWord(VERB_SERVICE),
+                getWord(ARTICLE_SERVICE),
+                getWord(ADJECTIVE_SERVICE),
+                getWord(NOUN_SERVICE));
     }
 
     private String getWord(final String serviceName) {
-        final List<ServiceInstance> list = discoveryClient.getInstances(serviceName);
-
-        if (Objects.nonNull(list) && !list.isEmpty()) {
-            final URI uri = list.get(0).getUri();
-
-            if (Objects.nonNull(uri)) {
-                return (new RestTemplate()).getForObject(uri, String.class);
-            }
-        }
-
-        return null;
+        return restTemplate.getForObject(HTTP_PREFIX + serviceName, String.class);
     }
 }
