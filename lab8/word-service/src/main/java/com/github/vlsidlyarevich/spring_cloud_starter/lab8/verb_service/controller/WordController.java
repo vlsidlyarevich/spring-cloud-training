09b141c488c56585b0baccba3441b76671a38b75
Uladzislau Sidliarevich <vlsidlyarevich@gmail.com>
2018-02-13 22:15:55 +0300

    lab-8 - implementation

--- lab8/word-service/src/main/java/com/github/vlsidlyarevich/spring_cloud_starter/lab8/verb_service/controller/WordController.java
+++ lab8/word-service/src/main/java/com/github/vlsidlyarevich/spring_cloud_starter/lab8/verb_service/controller/WordController.java
@@ -2,6 +2,7 @@ package com.github.vlsidlyarevich.spring_cloud_starter.lab8.verb_service.control
 
 import com.github.vlsidlyarevich.spring_cloud_starter.lab8.verb_service.model.WordResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
