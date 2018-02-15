62b972ad43093b8c95245abd1dfb8cd99aa1f9da
Uladzislau Sidliarevich <vlsidlyarevich@gmail.com>
2018-02-15 21:47:59 +0300

    lab-8 - done

--- lab8/word-service/src/main/java/com/github/vlsidlyarevich/spring_cloud_starter/lab8/verb_service/controller/WordController.java
+++ lab8/word-service/src/main/java/com/github/vlsidlyarevich/spring_cloud_starter/lab8/verb_service/controller/WordController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class WordController {
 
     private final String WORD_SEPARATOR = ",";
